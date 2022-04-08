49168b190930b222eff508564809075cf5b34312
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-04-08 03:44:11 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'ingehack-black':'#0d0d0d',
+      'ingehack-green': '#11b813',
+      'ingehack-white': '#fff',
+      'ingehack-gray': '#595959'
+    },
+    extend: {},
+
+  },
+  plugins: [],
+}
