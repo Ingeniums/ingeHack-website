37000a6c513d0e5f06411e7560bddafc08a31ca0
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-04-09 03:47:54 +0100

    fixed about section's title

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import SectionTitle from "./SectionTitle";
 const About = () => {
     return (
         <section className="section">
-            <SectionTitle title={"Abot"}/>
+            <SectionTitle title={"About"}/>
             <div className="flex flex-col gap-6 lg:flex-row">
                 <p className="text-ingehack-white flex-1 text-xl text-center md:text-2xl lg:text-left lg:text-3xl">
                 IngeHack is one of the greatest Ingnenium’s events, it's a CTF (Capture The Flag) competition that brings computer and information security geeks together in one place to have an amazing weekend taking on challenges, It also includes different conferences, presented by doctors and professors
