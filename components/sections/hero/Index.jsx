b65b571146c0d84f3c62d7229f23185729d36aaa
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-05-09 22:49:43 +0100

    added some margins to the hero section

--- components/sections/hero/Index.jsx
+++ components/sections/hero/Index.jsx
@@ -7,12 +7,12 @@ const Index = () => {
             <div className="flex flex-col items-center gap-10">
                 <div className="flex flex-col">
                         <h1 className={`font-Matrix mt-8 uppercase text-ingehack-green text-[3rem] sm:text-[5rem] lg:text-[8rem] ${style.glitchedTxt}`}>Ingehack</h1>
-                    <div className="flex justify-between items-center text-ingehack-white font-NeuHaasBold">
+                    <div className="flex justify-between items-center text-ingehack-white font-NeuHaasBold -mt-4 md:-mt-6 lg:-mt-10">
                         <h3>CTF Competition</h3>
                         <h3>3rd Edition</h3>
                     </div>
                 </div>
-                <div className="text-ingehack-white text-xl lg:text-2xl font-NeuHaasBold">
+                <div className="text-ingehack-white text-xl lg:text-2xl font-NeuHaasBold mt-2 lg:mt-6">
                     <h3>June XX-XX, 2022</h3>
                     <p>Exact adress, Oran</p>
                 </div>
