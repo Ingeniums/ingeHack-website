cb411ccbb724361a100308cb2a4164456813efe5
habs <abenchikh6@gmail.com>
2022-09-04 21:52:23 +0100

    changed about desc

--- components/sections/about/Index.jsx
+++ components/sections/about/Index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
             <SectionTitle title={"About"}/>
             <div className="flex flex-col items-center gap-6 lg:flex-row">
                 <p className="text-ingehack-white flex-1 text-xl text-center md:text-2xl lg:text-left lg:text-3xl font-NeuHaasRegular">
-                IngeHack is one of the greatest Ingnenium’s events, its a CTF (Capture The Flag) competition that brings computer and information security geeks together in one place to have an amazing weekend taking on challenges, It also includes different conferences, presented by doctors and professors
+                IngeHack is a largescale CTF (Capture The Flag) competition that brings computer and information security enthusiasts together for 24 hours of cybersecurity challenges.
                 </p>
                 <div className="flex-1">
                     <Image height={450} width={500} src="/ingehack_about.png" className="cursor-pointer " alt="CTF photo" />
